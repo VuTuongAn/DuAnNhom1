78ed7203c1d7493b81554078f59b3f2d93817aea
Vu Tuong An <vutuongan2003@gmail.com>
2022-11-03 13:36:31 +0700

    Tạo thêm nhánh

--- QuanLyCaPhe/src/main/java/com/mycompany/quanlycaphe/QuanLyCaPhe.java
+++ QuanLyCaPhe/src/main/java/com/mycompany/quanlycaphe/QuanLyCaPhe.java
@@ -14,5 +14,6 @@ public class QuanLyCaPhe {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello");
+        System.out.println("Thau đổi");
     }
 }
